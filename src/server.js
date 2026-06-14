7dffb8d8b7d205ad1130f370555702a1040408ec
Zayn <zaingu89@gmail.com>
2026-06-15 03:31:33 +0500

    chore: bump version to 2.1.2 to enable npm release

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ export async function startServer() {
   // --- Create MCP server ---
   const server = new McpServer({
     name: 'persyst',
-    version: '2.1.1'
+    version: '2.1.2'
   });
 
   // --- Register all tools ---
